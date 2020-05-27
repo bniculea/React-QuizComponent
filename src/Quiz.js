7fe78106d059673e47ee3a65e47f63c6fbd9fce3
Bogdan Niculeasa <bogdan.niculeasa@hotmail.com>
2020-05-27 11:12:55 +0300

    removed arrwo functions

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,7 +14,7 @@ class Quiz extends Component {
         }
     }
 
-    showNextQuestion = ()=> {
+    showNextQuestion(){
         this.setState({
             quiz_position: this.state.quiz_position + 1
         })
